ae0699f068ff10ed5dcab2cd633ebc44e8855efe
Bernardo Marujo <bernardomarujo@ua.pt>
2024-04-11 10:50:47 +0100

    default commit

--- AC2/test_p1_prep/ex1.c
+++ AC2/test_p1_prep/ex1.c
@@ -1,40 +1,29 @@
 #include <detpic32.h>
 
-void delay(unsigned int ms){
-    resetCoreTimer();
-    while(readCoreTimer() <  20000 * ms);
-    } 
-
-
-int main(){
-
-    TRISE= TRISE & 0xFF03; //output fica com 0
-    TRISB= TRISB | 0x0005; //input fica com 1
-    int count = 0x00C0; // count 0000 0000 1100 0000
-    LATE = (LATE & 0XFF03) | count; // colocar valor default 0000 0000 1100 0000
-    int p = 137;
-    int switches = 0;
-    //Padrão
-
-
+int main(void){
+    TRISE = (TRISE & 0xFFC3);             //XXXX XXXX XX00 00XX
+    TRISB = (TRISB | 0x0004);             //XXXX XXXX XXXX X1XX
+    int delay;
+    int count = 0;
     while(1){
-        switches = PORTB & 0x5; //variavel em que leio oq os switches têm
-        if(switches == 0) // 0000
-        {
-            p = 217;
+        LATE = (LATE & 0xFFC3)|(count<<2);
+
+        if (count == 0){
+            count = 11;
         }
-        else // 0101
-        { //o valor assumido pelos switches para a condição
-            p= 137;
+        else{
+            count = count - 1;
+            }
+
+        if(PORTBbits.RB2 == 1){
+            delay = 3636363;
         }
-        delay(p);
-        if (count== 0x000C){
-            count = 0x00C0;
-        } else {
-            count = count >> 1;
+        else if(PORTBbits.RB2 == 0){
+            delay = 8695652;
         }
-        //printf(count);        
-        LATE = (LATE & 0XFF03) | count; // colocar tudo a zeros e colocar o count que so tem dois '1's
+        printInt(count, 10 | 2 << 16);
+        putChar('\n');
+        resetCoreTimer();
+        while(readCoreTimer() < delay);
     }
-    return 0;
 }
--- AC2/test_p1_prep/ex2.c
+++ AC2/test_p1_prep/ex2.c
@@ -1,11 +1,6 @@
 #include <detpic32.h>
 
-unsigned char toBcd(unsigned char value)
-{
-    return ((value / 10) << 4) + (value % 10);
-}
-
-void send2displays(unsigned char value, int displayFlag) {
+void send2displays(unsigned char value ) {
     static const char disp7Scodes[] = {
         0x3F, 0x06, 0x5B, 0x4F,
         0x66, 0x6D, 0x7D, 0x07,
@@ -13,13 +8,10 @@ void send2displays(unsigned char value, int displayFlag) {
         0x39, 0x5E, 0x79, 0x71
     };
 
-    if (displayFlag) {
-        LATD = ( LATD & 0xFF9F ) | 0x0040; // Activate HI-D, disable LO-D
-        LATB = ( LATB & 0x80FF ) | disp7Scodes[value & 0x0F] << 8; // Write the values of RB8-RB14
-    } else {
-        LATD = ( LATD & 0xFFBF ) | 0x0020; // Disable HI-D, activate LO-D
-        LATB = ( LATB & 0x80FF ) | disp7Scodes[value & 0x0F] << 8; // Write the values of RB8-RB14
-    }
+
+    LATD = ( LATD & 0xFFBF ) | 0x0020; // Disable HI-D, activate LO-D
+    LATB = ( LATB & 0x80FF ) | disp7Scodes[value & 0x0F] << 8; // Write the values of RB8-RB14
+    
 }
 
 int main(void) {
@@ -28,39 +20,30 @@ int main(void) {
     AD1CON1bits.SSRC = 7;
     AD1CON1bits.CLRASAM = 1;
     AD1CON3bits.SAMC = 16;
-    AD1CON2bits.SMPI = 3;
+    AD1CON2bits.SMPI = 1;
     AD1CHSbits.CH0SA = 4;
     AD1CON1bits.ON = 1;
     TRISD = TRISD & 0xFF9F; 
     TRISB = TRISB & 0x80FF;
-    TRISBbits.TRISB1 = 1;
-    TRISEbits.TRISE4 = 0;
-    LATEbits.LATE4 = 0;
+    TRISEbits.TRISE1 = 0;
+    LATEbits.LATE1 = 0;
     while (1) {
         AD1CON1bits.ASAM = 1;
         while (IFS1bits.AD1IF == 0);
         int *p = (int *)(&ADC1BUF0);
-        int average = (p[0] + p[4] + p[8] + p[12]) / 4;
-        printStr("BUFFER: ");
-        printInt( p[0], 16 | 4 << 16);
-        putChar(' ');
-        printInt( p[4], 16 | 4 << 16);
-        putChar(' ');
-        printInt( p[8], 16 | 4 << 16);
-        putChar(' ');
-        printInt( p[12], 16 | 4 << 16);
+        int average = (p[0] + p[4]) / 2;
         printStr(" | AVERAGE: ");
-        printInt( average, 2 | 10 << 16);
+        printInt( average, 16 | 3 << 16);
         printStr(" | VOLTAGE: ");
         int voltage;
-        voltage = ( (average * 9 + 511) / 1023)+3;
+        voltage = ( (average * 9 + 511) / 1023);
         printInt(voltage, 16);
         putChar('\n');
-        send2displays((char) (voltage & 0x000000FF),PORTBbits.RB1);
+        send2displays((char) (voltage & 0x000000FF));
         IFS1bits.AD1IF = 0;
-        LATEbits.LATE4 = !LATEbits.LATE4;
+        LATEbits.LATE1 = !LATEbits.LATE1;
         resetCoreTimer();
-        while(readCoreTimer()<1666667);
+        while(readCoreTimer()<3333333);
         
 
     }
