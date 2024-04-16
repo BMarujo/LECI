30ec10176bec2b8d0d53948fdd5cb0bdb40e909f
Bernardo Marujo <bernardomarujo@ua.pt>
2024-04-16 22:35:20 +0100

    default commit

---
+++ COMPILADORES/pratica/guiao3/HTMLCompiler.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import org.stringtemplate.v4.*;
+
+@SuppressWarnings("CheckReturnValue")
+public class HTMLCompiler extends CSVBaseVisitor<ST> {
+
+   @Override public ST visitFile(CSVParser.FileContext ctx) {
+      ST res = templates.getInstanceOf("table");
+      
+      res.add("header", visit(ctx.header()).render());
+      
+      for (CSVParser.RowContext row : ctx.row()) {
+         res.add("row", visit(row).render());
+      }
+
+      return res;
+   }
+
+   @Override public ST visitHeader(CSVParser.HeaderContext ctx) {
+      ST res = templates.getInstanceOf("header");
+
+      for (CSVParser.HvalueContext hv : ctx.hvalue()) {
+         ST hvalue = templates.getInstanceOf("header_values");
+         hvalue.add("value", hv.getText());
+         res.add("header_values", hvalue.render());
+      }
+      return res;
+   }
+
+   @Override public ST visitRow(CSVParser.RowContext ctx) {
+      ST res = templates.getInstanceOf("rows");
+
+      for (CSVParser.ValueContext rv : ctx.value()) {
+         ST rvalue = templates.getInstanceOf("row_values");
+         rvalue.add("value", rv.getText());
+         res.add("row_values", rvalue.render());
+      }
+
+      return res;
+   }
+
+   private STGroup templates = new STGroupFile("html.stg");
+}
